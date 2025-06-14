68ac107290726d1a063efda96d38772f9747bb2e
Gabriel Spioni <gabrielprivate123@gmail.com>
2025-06-13 23:14:04 -0300

    feat: Criando exercício de pratica para consumo de endpoint e ordenação por LINQ

---
+++ exercise-2/gatorabs.HttpAndLinQ/Models/Music.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace gatorabs.HttpAndLinQ.Models
+{
+    public class Music
+    {
+
+        [JsonPropertyName("artist")]
+        public string Artist { get; set; }
+
+        [JsonPropertyName("song")]
+        public string Song { get; set; }
+
+        [JsonPropertyName("genre")]
+        public string Genre { get; set; }
+
+        public void ShowMusicDetail()
+        {
+            Console.WriteLine($"Artist: {Artist}");
+            Console.WriteLine($"Artist: {Song}");
+            Console.WriteLine($"Artist: {Genre}");
+            Console.WriteLine("");
+        }
+    }
+}
---
+++ exercise-2/gatorabs.HttpAndLinQ/Program.cs
@@ -0,0 +1,18 @@
+﻿using gatorabs.HttpAndLinQ.Models;
+using System.Text.Json;
+
+using (HttpClient client = new())
+{
+    var response = await client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
+    Console.WriteLine(response);
+    var musicas = JsonSerializer.Deserialize<List<Music>>(response);
+    var musicasOrdem = musicas.OrderBy(m => m.Artist);
+    var musicasOrdemGenre = musicas.OrderBy(m => m.Artist == "Eminem");
+    musicasOrdem.ToList<Music>();
+
+    foreach (Music musica in musicasOrdemGenre)
+    {
+        musica.ShowMusicDetail();
+        Console.WriteLine("");
+    }
+}
