8d39bcbd6d2cb1e00b6c35bc852405f636687ea3
Gabriel Spioni <gabrielprivate123@gmail.com>
2025-06-13 23:44:30 -0300

    feat/fix: Mudanças de diretório e criando Exercício utilizando filtro Where.

---
+++ exercise-2/usingWhere/gatorabs.HttpAndLinQ-2/Models/Movie.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace gatorabs.HttpAndLinQ_2.Models
+{
+    public class Movie
+    {
+
+        [JsonPropertyName("rank")]
+        public string Rank { get; set; }
+
+        [JsonPropertyName("title")]
+        public string Title { get; set; }
+
+        [JsonPropertyName("year")]
+        public string Year { get; set; }
+
+        [JsonPropertyName("image")]
+        public string Image { get; set; }
+
+
+        public void ShowMovieDetail()
+        {
+            Console.WriteLine($"Movie: {Title}");
+            Console.WriteLine($"Rank: {Rank}");
+            Console.WriteLine($"Year: {Year}");
+            Console.WriteLine($"Image: {Image}");
+            Console.WriteLine("");
+        }
+    }
+}
---
+++ exercise-2/usingWhere/gatorabs.HttpAndLinQ-2/Program.cs
@@ -0,0 +1,15 @@
+﻿using gatorabs.HttpAndLinQ_2.Models;
+using System.Text.Json;
+
+using (HttpClient client = new())
+{
+    var response = await client.GetStringAsync("https://raw.githubusercontent.com/ArthurOcFernandes/Exerc-cios-C-/curso-4-aula-2/Jsons/TopMovies.json");
+    var movies = JsonSerializer.Deserialize<List<Movie>>(response);
+
+    var orderedMovies = movies.Where(o => Int32.Parse(o.Year) >= 2000);
+    foreach (Movie movie in orderedMovies)
+    {
+        movie.ShowMovieDetail();
+    }
+
+}
